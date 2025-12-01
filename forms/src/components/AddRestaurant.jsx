859ad40aa89110d272a010b669c70ba62be7834e
P Cisneros <psy@psysviewfinder.com>
2025-11-30 20:25:52 -0800

    Added onChange, setFormData, update state

--- forms/src/components/AddRestaurant.jsx
+++ forms/src/components/AddRestaurant.jsx
@@ -1,35 +1,95 @@
 import { useState } from "react";
+
 function AddRestaurant({ updateRestaurants }) {
+  const [formData, setFormData] = useState({
+    name: "",
+    address: "",
+    phone: "",
+    cuisine: "",
+    rating: "",
+    image: ""
+  });
+
+  // This function will handle the change event for our form.
+  const handleChange = (e) => {
+    const name = e.target.name
+    const value = e.target.value
+    // Here we are copying in the current form data with the spread operator
+    // We are then adding the new key value pair with [name]:value
+    setFormData({ ...formData, [name]: value });
+  };
+
+  // Test the state updates
+  console.log(formData);
+
   return (
     <form>
       <div>
         <label>Name:</label>
-        <input type="text" name="name" required />
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
       </div>
 
       <div>
         <label>image:</label>
-        <input type="text" name="image" required />
+        <input
+          type="text"
+          name="image"
+          value={formData.image}
+          onChange={handleChange}
+          required
+        />
       </div>
 
       <div>
         <label>Address:</label>
-        <input type="text" name="address" required />
+        <input
+          type="text"
+          name="address"
+          value={formData.address}
+          onChange={handleChange}
+          required
+        />
       </div>
 
       <div>
         <label>Phone:</label>
-        <input type="tel" name="phone" required />
+        <input
+          type="tel"
+          name="phone"
+          value={formData.phone}
+          onChange={handleChange}
+          required
+        />
       </div>
 
       <div>
         <label>Cuisine:</label>
-        <input type="text" name="cuisine" required />
+        <input
+          type="text"
+          name="cuisine"
+          value={formData.cuisine}
+          onChange={handleChange}
+          required
+        />
       </div>
 
       <div>
         <label>Rating:</label>
-        <input type="number" name="rating" min="1" max="5" required />
+        <input
+          type="number"
+          name="rating"
+          min="1"
+          max="5"
+          value={formData.rating}
+          onChange={handleChange}
+          required
+        />
       </div>
 
       <button type="submit">Submit</button>
